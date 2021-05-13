7429b937e35c93a68094a5bae5d8286301f9acc4
adriana <adrianafs1991@gmail.com>
2021-05-13 16:00:11 -0500

    añado index.js con el inmutable

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ console.log(
     map1.get('b') + " vs. " + map2.get('b')
     );
     // 2 vs. 50
+
+
+const { Map } = require('immutable');
+const map1 = Map({ a: 1, b: 2, c: 3 });
+const map2 = Map({ a: 1, b: 2, c: 3 });
+console.log(map1.equals(map2)); // true
+console.log(map1 === map2); // false
